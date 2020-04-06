87db64bebe6c50dfd8e5b01ed0e5e9c246a2715f
misohan <michal.hanusniak@aiesec.cz>
2020-04-06 20:07:16 +0200

    Create product fields

--- src/main/java/codecool/mma/Product.java
+++ src/main/java/codecool/mma/Product.java
@@ -1,4 +1,12 @@
 package codecool.mma;
 
 public class Product {
+    private int ProductID;
+    private String brand;
+    private String model;
+    private String type;
+    private int price;
+    private int size;
+    private boolean availability;
+
 }
