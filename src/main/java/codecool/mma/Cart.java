82085a19c4504af8247d552b17352b6f136d0f71
martin-devp <martinnzakk@gmail.com>
2020-04-12 17:47:55 +0200

    Update Cart.java, Order.java

--- src/main/java/codecool/mma/Cart.java
+++ src/main/java/codecool/mma/Cart.java
@@ -5,4 +5,37 @@ public class Cart {
     private int quantity;
     private int totalCost;
     private boolean readyForCheckout;
+
+    public int getProductID() {
+        return ProductID;
+    }
+
+    public void setProductID(int ProductID) {
+        this.ProductID = ProductID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public boolean getReadyForCheckout() {
+        return readyForCheckout;
+    }
+
+    public void setReadyForCheckout(boolean readyForCheckout) {
+        this.readyForCheckout = readyForCheckout;
+    }
+
 }
