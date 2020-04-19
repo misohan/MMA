e4ece83ee364734005363fa2eefdec79e7a13414
misohan <michal.hanusniak@aiesec.cz>
2020-04-19 16:43:36 +0200

    Update Main

--- src/main/java/codecool/mma/Main.java
+++ src/main/java/codecool/mma/Main.java
@@ -5,19 +5,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        CustomerService customerService = new CustomerService();
-        ProductDAO productDAO = new ProductDAO();
-        UserController userController = new UserController();
-
-        Admin admin = new Admin();
-
         ServiceController serviceController = new ServiceController();
         serviceController.sayHello();
         serviceController.selectUserOrAdmin();
         serviceController.adminOptions();
-
-
-        customerService.customersOptions(productDAO);
 
 
 
